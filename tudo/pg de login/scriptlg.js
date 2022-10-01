fe255773710a415e285550704f41c43bef25969f
André Coêlho <andrelucascoelho281@gmail.com>
2022-10-01 19:17:21 -0300

    novo forms para calculos

--- tudo/pg de login/scriptlg.js
+++ tudo/pg de login/scriptlg.js
@@ -1,45 +1,9 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-
-form.addEventListener('submite', (e) => {
-
-    e.preventDefault();
-
-    checkInputs();
-})
-
-function checkInputs() {
-    const usernameValue = username.value.trim()
-    const passwordValue = password.value.trim()
-
-    if(usernameValue === ''){
-
-        errorValidation(username, "Preencha esse campo!")
-
-    } else{
-        sucessValidation(username)
-
-    }
-
-    if(passwordValue === '') {
-        errorValidation(password, "Preencha esse campo!")
-    } else{
-        sucessValidation(password)
-    }
-}
-
-function errorValidation(input, message) {
-    const inputfieldError = input.parentElement;
-    const small = inputfieldError.querySelector('small')
-
-    small.innerText = message;
-
-    inputfieldError.className = 'input-field-error'
-}
-
-function sucessValidation() {
-    const inputfieldSucess = input.parentElement;
-
-    inputfieldSucess.className = 'input-fiel-sucess'
-}
+const button = document.querySelector(".button");
+button.addEventListener("click", () =>{
+  button.classList.add("active");
+  setTimeout(()=>{
+    button.classList.remove("active");
+    button.querySelector("i").classList.replace("bx-cloud-download", "bx-check-circle")
+    button.querySelector("span").innerText = "Completed";
+  },6000);
+});
