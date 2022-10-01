a6cc25d41224ebb04df2d3d6171dab76a0e31444
André Coêlho <andrelucascoelho281@gmail.com>
2022-09-30 23:43:10 -0300

    novo desing de forms

---
+++ tudo/testes.js
@@ -0,0 +1,9 @@
+const button = document.querySelector(".button");
+button.addEventListener("click", () =>{
+  button.classList.add("active");
+  setTimeout(()=>{
+    button.classList.remove("active");
+    button.querySelector("i").classList.replace("bx-cloud-download", "bx-check-circle")
+    button.querySelector("span").innerText = "Completed";
+  },6000);
+});
