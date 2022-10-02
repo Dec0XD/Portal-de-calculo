8e31c21a8b8e5647714d04c861a9b97c4452cfdc
André Coêlho <andrelucascoelho281@gmail.com>
2022-10-01 21:46:47 -0300

    somente testes

--- tudo/pg de login/scriptlg.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".button");
-button.addEventListener("click", () =>{
-  button.classList.add("active");
-  setTimeout(()=>{
-    button.classList.remove("active");
-    button.querySelector("i").classList.replace("bx-cloud-download", "bx-check-circle")
-    button.querySelector("span").innerText = "Completed";
-  },6000);
-});
