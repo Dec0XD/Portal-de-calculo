f5b054301bbc50ce23bae70cd5cdbfd9205fadff
João <joaogabrielsj2004@gmail.com>
2022-10-20 18:09:38 -0300

    checkbox p/ páginas

--- calculo.js
+++ calculo.js
@@ -46,3 +46,35 @@ campo_opcao.addEventListener('change', function () {
 		document.getElementById("opt_D").style = "display:block;"
 	}
 })
+
+var button_cilindrical = document.getElementById("cilindrical_button")
+var button_conical = document.getElementById("conical_button")
+var button_spherical = document.getElementById("spherical_button")
+var i = 0;
+var j = 0;
+var k = 0;
+button_cilindrical.addEventListener('change', function() {
+	i++
+	console.log(i)
+	if(i%2==0){
+		document.getElementById("cilindrical_page").style = "display:none;"
+	} else {
+		document.getElementById("cilindrical_page").style = "display:flex;"
+	}
+})
+button_conical.addEventListener('change', function() {
+	j++
+	if(j%2==0){
+		document.getElementById("conical_page").style = "display:none;"
+	} else {
+		document.getElementById("conical_page").style = "display:flex;"
+	}
+})
+button_spherical.addEventListener('change', function() {
+	k++
+	if(k%2==0){
+		document.getElementById("spherical_page").style = "display:none;"
+	} else {
+		document.getElementById("spherical_page").style = "display:flex;"
+	}
+})
