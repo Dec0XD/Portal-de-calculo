29f7786f23b7015ba6f404ef84d11b6e191e436a
André Coêlho <andrelucascoelho281@gmail.com>
2022-10-21 09:52:17 -0300

    att responsividade

--- Testes/testes.js
+++ Testes/testes.js
@@ -35,3 +35,14 @@ function openModal(){
 function closeModal() {
 	modal.classList.remove('active')
 }
+
+//contas - teste//
+
+var num1 = document.querySelector("#num1");
+var num2 = document.querySelector("#num2");
+var resultado = document.querySelector("#resp")
+
+function media(){
+	resultado.innerHTML = (parseFloat(num1.value) + parseFloat(num2.value))/2
+}
+
