8dd211293ac5da8122b9055a461a77d67d92d890
André Coêlho <andrelucascoelho281@gmail.com>
2022-11-25 23:51:31 -0300

    add

--- calculo.js
+++ calculo.js
@@ -158,3 +158,17 @@
      	})
 
      })
+	 
+	 function up(max) {
+		document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+		if (document.getElementById("myNumber").value >= parseInt(max)) {
+			document.getElementById("myNumber").value = max;
+		}
+	}
+	function down(min) {
+		document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+		if (document.getElementById("myNumber").value <= parseInt(min)) {
+			document.getElementById("myNumber").value = min;
+		}
+	}
+	
