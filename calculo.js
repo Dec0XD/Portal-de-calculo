98a4ef162ccf7e56df7663979f65c77312d1b106
André Coêlho <andrelucascoelho281@gmail.com>
2023-01-04 18:38:38 -0300

    setas no js

--- calculo.js
+++ calculo.js
@@ -43,76 +43,96 @@
 
      var button_cilindrical = document.getElementById("cilindrical_button")
      var button_conical = document.getElementById("conical_button")
-	 var button_spherical = document.getElementById("spherical_button")
-	 var button_ellipsoidal = document.getElementById("ellipsoidal_button")
-	 var button_TORISPHERICAL = document.getElementById("TORISPHERICAL_button")
+     var button_spherical = document.getElementById("spherical_button")
+     var button_ellipsoidal = document.getElementById("ellipsoidal_button")
+     var button_TORISPHERICAL = document.getElementById("TORISPHERICAL_button")
      var button_header = document.getElementById("btn-print-pdf")
      var i = 0;
      var j = 0;
      var k = 0;
-	 var l = 0;
-	 var m = 0;
-	 var n = 0;
-	 button_header.addEventListener('change', function () {
-		l++
-		if (l % 2 == 0) {
-			document.getElementById("titulo").style = "display:block;"
-		} else {
-			document.getElementById("titulo").style = "display:none;"
+     var l = 0;
+     var m = 0;
+     var n = 0;
+     button_header.addEventListener('change', function () {
+     	l++
+     	if (l % 2 == 0) {
+     		document.getElementById("titulo").style = "display:block;"
+     	} else {
+     		document.getElementById("titulo").style = "display:none;"
 
-		}
-	 })
+     	}
+     })
      button_cilindrical.addEventListener('change', function () {
      	i++
      	console.log(i)
      	if (i % 2 == 0) {
      		document.getElementById("cilindrical_page").style = "display:none;"
      		document.getElementById("cilindrical_page2").style = "display:none;"
+     		document.getElementById("cilindricalArrow").style = "display:none"
+     		document.getElementById("cilindricalArrow2").style = "display:none"
      	} else {
      		document.getElementById("cilindrical_page").style = "display:flex;"
      		document.getElementById("cilindrical_page2").style = "display:flex;"
+     		document.getElementById("cilindricalArrow").style = "display:flex"
+     		document.getElementById("cilindricalArrow2").style = "display:flex"
      	}
      })
      button_conical.addEventListener('change', function () {
      	j++
      	if (j % 2 == 0) {
      		document.getElementById("conical_page").style = "display:none;"
-     		document.getElementById("conical_page2").style = "display:none;"
+			  document.getElementById("conical_page2").style = "display:none;"
+			  document.getElementById("conicalArrow").style = "display:none"
+			  document.getElementById("conicalArrow2").style = "display:none"
      	} else {
      		document.getElementById("conical_page").style = "display:flex;"
-     		document.getElementById("conical_page2").style = "display:flex;"
+			  document.getElementById("conical_page2").style = "display:flex;"
+			  document.getElementById("conicalArrow").style = "display:flex"
+			  document.getElementById("conicalArrow2").style = "display:flex"
      	}
      })
      button_spherical.addEventListener('change', function () {
      	k++
      	if (k % 2 == 0) {
      		document.getElementById("SPHERICAL_page").style = "display:none;"
-     		document.getElementById("SPHERICAL_page2").style = "display:none;"
+			  document.getElementById("SPHERICAL_page2").style = "display:none;"
+			  document.getElementById("sphericalArrow").style = "display:none"
+			  document.getElementById("sphericalArrow2").style = "display:none"
      	} else {
      		document.getElementById("SPHERICAL_page").style = "display:flex;"
-     		document.getElementById("SPHERICAL_page2").style = "display:flex;"
+			  document.getElementById("SPHERICAL_page2").style = "display:flex;"
+			  document.getElementById("sphericalArrow").style = "display:flex"
+			  document.getElementById("sphericalArrow2").style = "display:flex"
      	}
-	 })
-	 button_ellipsoidal.addEventListener('change', function () {
-		m++
-		if (m % 2 == 0) {
-			document.getElementById("ELLIPSOIDAL_HEADS_page").style = "display:none;"
-			document.getElementById("ELLIPSOIDAL_HEADS_page2").style = "display:none;"
-		} else {
-			document.getElementById("ELLIPSOIDAL_HEADS_page").style = "display:flex;"
-			document.getElementById("ELLIPSOIDAL_HEADS_page2").style = "display:flex;"
-		}
-	 })
-	 button_TORISPHERICAL.addEventListener('change', function () {
-		n++
-		if (n % 2 == 0) {
-			document.getElementById("TORISPHERICAL_HEADS_page").style = "display:none;"
-			document.getElementById("TORISPHERICAL_HEADS_page2").style = "display:none;"
-		} else {
-			document.getElementById("TORISPHERICAL_HEADS_page").style = "display:flex;"
-			document.getElementById("TORISPHERICAL_HEADS_page2").style = "display:flex;"
-		}
-	})
+     })
+     button_ellipsoidal.addEventListener('change', function () {
+     	m++
+     	if (m % 2 == 0) {
+     		document.getElementById("ELLIPSOIDAL_HEADS_page").style = "display:none;"
+			  document.getElementById("ELLIPSOIDAL_HEADS_page2").style = "display:none;"
+			  document.getElementById("ellipsoidalArrow").style = "display:none"
+			  document.getElementById("ellipsoidalArrow2").style = "display:none"
+     	} else {
+     		document.getElementById("ELLIPSOIDAL_HEADS_page").style = "display:flex;"
+			  document.getElementById("ELLIPSOIDAL_HEADS_page2").style = "display:flex;"
+			  document.getElementById("ellipsoidalArrow").style = "display:flex"
+			  document.getElementById("ellipsoidalArrow2").style = "display:flex"
+     	}
+     })
+     button_TORISPHERICAL.addEventListener('change', function () {
+     	n++
+     	if (n % 2 == 0) {
+     		document.getElementById("TORISPHERICAL_HEADS_page").style = "display:none;"
+			  document.getElementById("TORISPHERICAL_HEADS_page2").style = "display:none;"
+			  document.getElementById("torisphericalArrow").style = "display:none;"
+			  document.getElementById("torisphericalArrow2").style = "display:none;"
+     	} else {
+     		document.getElementById("TORISPHERICAL_HEADS_page").style = "display:flex;"
+			  document.getElementById("TORISPHERICAL_HEADS_page2").style = "display:flex;"
+			  document.getElementById("torisphericalArrow").style = "display:flex;"
+			  document.getElementById("torisphericalArrow2").style = "display:flex;"
+     	}
+     })
 
 
 
@@ -120,7 +140,7 @@
      //print//
      $('#btn-print-pdf').click(function () {
      	document.getElementById("divheader").style = "display: none;"
-		document.getElementById("sectionFinalpage").style = "display: none;"
+     	document.getElementById("sectionFinalpage").style = "display: none;"
 
      	$(".container").hide();
      	$('#pdfDocument').attr('src', 'https://connect-hm.top/pdf/test.pdf');
@@ -129,7 +149,7 @@
      		$(".container").show();
      	}, 1000);
 
-		document.getElementById("sectionFinalpage").style = "display: block;"
+     	document.getElementById("sectionFinalpage").style = "display: block;"
      	document.getElementById("divheader").style = "display: block;"
      });
 
@@ -164,7 +184,7 @@
      			// SUCCESS
      			res.json().then(data => {
      				const yourData = data
-					console.log(yourData)
+     				console.log(yourData)
      			}).catch(err => console.log(err))
      		} else {
      			// ERROR
@@ -172,25 +192,29 @@
      	})
 
      })
-	 
-	 function up(max) {
-		document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
-		if (document.getElementById("myNumber").value >= parseInt(max)) {
-			document.getElementById("myNumber").value = max;
-		}
-	}
-	function down(min) {
-		document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
-		if (document.getElementById("myNumber").value <= parseInt(min)) {
-			document.getElementById("myNumber").value = min;
-		}
-}
-	
+
+     function up(max) {
+     	document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+     	if (document.getElementById("myNumber").value >= parseInt(max)) {
+     		document.getElementById("myNumber").value = max;
+     	}
+     }
+
+     function down(min) {
+     	document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+     	if (document.getElementById("myNumber").value <= parseInt(min)) {
+     		document.getElementById("myNumber").value = min;
+     	}
+     }
 
 
 
-const selection = document.querySelector('.section')
 
-selection = addEventListener('click', () => {
-	window.scroll({top: window.innerHeight, behavior: "smooth"})
-})
+     const selection = document.querySelector('.section')
+
+     selection = addEventListener('click', () => {
+     	window.scroll({
+     		top: window.innerHeight,
+     		behavior: "smooth"
+     	})
+     })
